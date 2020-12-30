bb2a22f29a4b4e9b229b6782352b25606586f037
Louis Michael Medina <viewsdevelop@gmail.com>
2020-12-30 01:10:08 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,14 @@
+// Assign an object to the variable playlist and initialize the object with a
+// key-value pair — the keys will be artist names and the values will be song
+// titles. (What limitation does this impose on our playlist?)
+
+var playlist = new Object({ sinatra: my way });
+
+// Create a function updatePlaylist that accepts three parameters: the playlist
+// (an object), an artist name (a string), and a song title. The body of the
+// function should add the song and artist as a key-value pair to the playlist
+// object. The function should return the whole playlist.
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return new Object({}, { artistName: songTitle });
+}
